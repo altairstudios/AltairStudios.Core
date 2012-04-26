17105b52093bf97ff44784bb91ea45835c0e7878
Juan Benavides Romero <jbalde@gmail.com>
2012-04-26 02:08:35 +0200

    Create a void class and interface for future implementation of plugin system.

---
+++ AltairStudios.Core/Plugin/PluginBase.cs
@@ -0,0 +1,15 @@
+using System;
+
+
+namespace AltairStudios.Core.Plugin {
+	/// <summary>
+	/// Plugin base.
+	/// </summary>
+	public class PluginBase {
+		/// <summary>
+		/// Initializes a new instance of the <see cref="AltairStudios.Core.Plugin.PluginBase"/> class.
+		/// </summary>
+		public PluginBase() {
+		}
+	}
+}
---
+++ AltairStudios.Core/Plugin/iPlugin.cs
@@ -0,0 +1,11 @@
+using System;
+
+
+namespace AltairStudios.Core.Plugin {
+	/// <summary>
+	/// Interface for plugins.
+	/// </summary>
+	public interface iPlugin {
+		
+	}
+}
