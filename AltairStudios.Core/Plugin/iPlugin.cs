325ed863ecefb34da955030cd9114f273f46aa03
Juan Benavides Romero <jbalde@gmail.com>
2012-04-27 00:55:45 +0200

    Defined base plugin info name, description and version

--- AltairStudios.Core/Plugin/iPlugin.cs
+++ AltairStudios.Core/Plugin/iPlugin.cs
@@ -6,6 +6,41 @@ namespace AltairStudios.Core.Plugin {
 	/// Interface for plugins.
 	/// </summary>
 	public interface iPlugin {
+		/// <summary>
+		/// Gets or sets the name.
+		/// </summary>
+		/// <value>
+		/// The name.
+		/// </value>
+		string Name {
+			get;
+			set;
+		}
 		
+		
+		
+		/// <summary>
+		/// Gets or sets the version.
+		/// </summary>
+		/// <value>
+		/// The version.
+		/// </value>
+		string Version {
+			get;
+			set;
+		}
+		
+		
+		
+		/// <summary>
+		/// Gets or sets the description.
+		/// </summary>
+		/// <value>
+		/// The description.
+		/// </value>
+		string Description {
+			get;
+			set;
+		}
 	}
 }
