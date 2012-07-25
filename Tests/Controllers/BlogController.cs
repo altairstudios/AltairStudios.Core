af2ac8731c148234a9516e43fea5cafaade9873c
Juan Benavides Romero <jbalde@gmail.com>
2012-07-25 16:15:03 +0200

    Added basic home blog

--- Tests/Controllers/BlogController.cs
+++ Tests/Controllers/BlogController.cs
@@ -5,9 +5,22 @@ using AltairStudios.Core.Tests.Web.Models.Blog;
 
 namespace AltairStudios.Core.Tests.Web.Controllers {
 	public class BlogController : Controller {
+		protected ModelList<Category> categories;
+		ModelList<Tag> tags;
+		
+		protected BlogController() {
+			Category category = new Category();
+			Tag tag = new Tag();
+
+			this.categories = category.getBy<Category>();
+			this.tags = tag.getBy<Tag>();
+			
+			ViewData["categories"] = this.categories;
+			ViewData["tags"] = this.tags;
+		}
+		
 		public ActionResult Index() {
 			Post post = new Post();
-			
 			ModelList<Post> posts = post.getBy<Post>(true);
 			
 			ViewData["posts"] = posts;
