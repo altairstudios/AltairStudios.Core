95d48ded06f6e605c6e34538213ef1fc5a4e5404
Juan Benavides Romero <jbalde@gmail.com>
2012-07-26 02:43:33 +0200

    Added home blog functional

--- Tests/Controllers/BlogController.cs
+++ Tests/Controllers/BlogController.cs
@@ -4,22 +4,10 @@ using AltairStudios.Core.Tests.Web.Models.Blog;
 
 
 namespace AltairStudios.Core.Tests.Web.Controllers {
-	public class BlogController : Controller {
-		protected ModelList<Category> categories;
-		ModelList<Tag> tags;
-		
-		protected BlogController() {
-			Category category = new Category();
-			Tag tag = new Tag();
-
-			this.categories = category.getBy<Category>();
-			this.tags = tag.getBy<Tag>();
-			
-			ViewData["categories"] = this.categories;
-			ViewData["tags"] = this.tags;
-		}
-		
+	public class BlogController : Controller {		
 		public ActionResult Index() {
+			this.loadCommonData();
+			
 			Post post = new Post();
 			ModelList<Post> posts = post.getBy<Post>(true);
 			
@@ -29,15 +17,32 @@ namespace AltairStudios.Core.Tests.Web.Controllers {
 		}
 		
 		public ActionResult Post() {
+			this.loadCommonData();
+			
 			return View();
 		}
 		
 		public ActionResult Category() {
+			this.loadCommonData();
+			
 			return View();
 		}
 		
 		public ActionResult Tag() {
+			this.loadCommonData();
+			
 			return View();
+		}
+		
+		protected void loadCommonData() {
+			Category category = new Category();
+			Tag tag = new Tag();
+
+			ModelList<Category> categories = category.getBy<Category>();
+			ModelList<Tag> tags = tag.getBy<Tag>();
+			
+			ViewData["categories"] = categories;
+			ViewData["tags"] = tags;
 		}
 	}
 }
